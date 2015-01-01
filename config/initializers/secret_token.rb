82b8372d033d27498a97fa724883af731effc7bf
Ashwin Thakur <ashesnz@gmail.com>
2015-01-01 13:25:55 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prism::Application.config.secret_key_base = '451ffd450f926384c63a9df86fa0e2d137859d4f5009c20dc0b22a9be77ac7955db4b36de9a70b68fb1b2e25950c420c70c6f272f9b5ec4dde976ae82856dd8a'
